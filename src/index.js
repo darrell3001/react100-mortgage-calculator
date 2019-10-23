60384bb5cad3074d89b37d5f421de9d8e463ed02
Darrell Sturdivant <darrell3001@gmail.com>
2019-10-22 17:16:57 -0700

    Fix output decimal point and remove console.log lines

--- src/index.js
+++ src/index.js
@@ -11,12 +11,4 @@ ReactDOM.render(<SelectComponent />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
-
-//repo
-// import React, { Component } from "react";
-// import ReactDOM from "react-dom";
-// import { render } from "react-dom";
-// import App from "./app";
-
-// ReactDOM.render(<SelectComponent />, document.getElementById("root"));
+serviceWorker.unregister();
