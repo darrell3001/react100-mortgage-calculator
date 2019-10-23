3e4b8a3d0fe7ac6c598162ff87d50ae4a88eeb7d
Darrell Sturdivant <darrell3001@gmail.com>
2019-10-22 17:42:09 -0700

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import SelectComponent from "./App";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<SelectComponent />, document.getElementById("root"));
+
